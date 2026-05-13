0cd7f59c2ed8e7875e287d275d08367e57098cf4
Your Name <_GoLdEn_BlAcK_X>
2026-05-13 18:26:10 +0500

    chore: init vue 3 + ts project

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsConfig from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,cjs,ts,mts,vue}'],
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsConfig(),
+  skipFormatting,
+]
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <!-- Место для ссылок -->
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
